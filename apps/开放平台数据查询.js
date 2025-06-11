34aa2a07882bafc75dc289856c8f4ebe85db8f99
小丞 <jiyucheng2001@foxmail.com>
2025-06-11 22:18:21 +0800

    refactor: 优化模板详情回复按钮的代码结构

--- apps/开放平台数据查询.js
+++ apps/开放平台数据查询.js
@@ -423,13 +423,14 @@ ${targetTemplate.text || '无内容'}`
       }
     })
 
-
-    return await e.reply([image, segment.button([
+    const button = segment.button(
       [
         { text: '返回列表', callback: 'bot模板', clicked_text: '正在返回列表' },
         { text: '复制模板', input: templateDetail, clicked_text: '正在复制模板' }
       ]
-    ])])
+    )
+
+    return await e.reply([image, button])
   }
 }
 
